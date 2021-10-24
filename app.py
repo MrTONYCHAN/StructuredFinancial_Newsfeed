0b42ae50877a90471d7d9475b48560307984983a
CHANDAN KUMAR <70132200+MrTONYCHAN@users.noreply.github.com>
2021-10-24 12:20:01 +0530

    Update app.py
    !

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-!pip install -U pip setuptools wheel
-!pip install -U spacy
-!python -m spacy download en_core_web_sm
+pip install -U pip setuptools wheel
+pip install -U spacy
+python -m spacy download en_core_web_sm
 
 import pandas as pd
 import requests
